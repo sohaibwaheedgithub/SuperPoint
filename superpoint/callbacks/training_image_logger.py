a3afa984b4db8a0bea7d7f83971f13c9f7d566bc
Sohaib Waheed <expk0322@pakoxygen.com>
2026-02-14 09:20:57 +0500

    remove .clip_by_value from images

--- superpoint/callbacks/training_image_logger.py
+++ superpoint/callbacks/training_image_logger.py
@@ -12,7 +12,7 @@ class TrainingImageLogger(keras.callbacks.Callback):
 
     def on_epoch_end(self, epoch, logs=None):
         outputs = self.model(self._images, training=False)
-        images = tf.clip_by_value(self._images, 0.0, 1.0)
+        images = self._images
         heatmaps = tf.clip_by_value(outputs["heatmap"], 0.0, 1.0)
 
         with self._writer.as_default():
