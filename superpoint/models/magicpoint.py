4e08212bd75a9b912fed68cfe3a1f3dac303a1c2
Sohaib Waheed <expk0322@pakoxygen.com>
2026-02-21 07:39:26 +0500

    added grads and weights in logs

--- superpoint/models/magicpoint.py
+++ superpoint/models/magicpoint.py
@@ -13,9 +13,9 @@ class MagicPoint(keras.Model):
     def __init__(self, mean, variance):
         super().__init__()
 
-        self.encoder = SharedEncoder()
-        self.decoder = Decoder(65)
-        self.post = DetectorPostProcessor()
+        self.encoder = SharedEncoder(name="shared_encoder")
+        self.decoder = Decoder(65, name="decoder")
+        self.post = DetectorPostProcessor(name="detector_post_processor")
         self.cdap_metric = CornerDetectionAveragePrecision(
             name="corner_detection_average_precision"
         )
@@ -39,6 +39,12 @@ class MagicPoint(keras.Model):
             "heatmap": heatmap,
         }
 
+    def _safe_global_norm(self, tensors):
+        valid_tensors = [t for t in tensors if t is not None]
+        if not valid_tensors:
+            return tf.constant(0.0, dtype=tf.float32)
+        return tf.linalg.global_norm(valid_tensors)
+
 
 
     def train_step(self, data):
@@ -54,9 +60,38 @@ class MagicPoint(keras.Model):
         grads = tape.gradient(loss, self.trainable_variables)
         self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
         
+
+
+        encoder_grads, decoder_grads = [], []
+        encoder_weights, decoder_weights = [], []
+
+        for grad, var in zip(grads, self.trainable_variables):
+            if "shared_encoder" in var.name:
+                encoder_weights.append(var)
+                if grad is not None:
+                    encoder_grads.append(grad)
+            elif "decoder" in var.name:
+                decoder_weights.append(var)
+                if grad is not None:
+                    decoder_grads.append(grad)
+
         self.cdap_metric.update_state(data["points"], outputs["heatmap"])
+
+        valid_grads = [g for g in grads if g is not None]
+
+        global_grad_norm = tf.linalg.global_norm(valid_grads) if valid_grads else tf.constant(0.0, tf.float32)
+        encoder_grad_norm = tf.linalg.global_norm(encoder_grads) if encoder_grads else tf.constant(0.0, tf.float32)
+        decoder_grad_norm = tf.linalg.global_norm(decoder_grads) if decoder_grads else tf.constant(0.0, tf.float32)
+        encoder_weight_norm = tf.linalg.global_norm(encoder_weights) if encoder_weights else tf.constant(0.0, tf.float32)
+        decoder_weight_norm = tf.linalg.global_norm(decoder_weights) if decoder_weights else tf.constant(0.0, tf.float32)
+
         return {
             "loss": loss,
+            "grads/global_norm": global_grad_norm,
+            "grads/encoder_norm": encoder_grad_norm,
+            "grads/decoder_norm": decoder_grad_norm,
+            "weights/encoder_norm": encoder_weight_norm,
+            "weights/decoder_norm": decoder_weight_norm,
             **self.cdap_metric.result()
         }
 
@@ -72,7 +107,22 @@ class MagicPoint(keras.Model):
         )
 
         self.cdap_metric.update_state(data["points"], outputs["heatmap"])
+
+        encoder_weights, decoder_weights = [], []
+
+        for var in self.trainable_variables:
+            if "shared_encoder" in var.name:
+                encoder_weights.append(var)
+            elif "decoder" in var.name:
+                decoder_weights.append(var)
+
+        encoder_weight_norm = tf.linalg.global_norm(encoder_weights) if encoder_weights else tf.constant(0.0, tf.float32)
+        decoder_weight_norm = tf.linalg.global_norm(decoder_weights) if decoder_weights else tf.constant(0.0, tf.float32)
+
+
         return {
             "loss": loss,
+            "weights/encoder_norm": encoder_weight_norm,
+            "weights/decoder_norm": decoder_weight_norm,
             **self.cdap_metric.result()
         }
