bf73a3fd20e6b54a8ada9e0b4a77889c364496ae
sohaibwaheedgithub <sohaibwaheedgithub.mle@gmail.com>
2026-05-24 18:51:31 +0500

    ddd

--- superpoint/training/train_magicpoint.py
+++ superpoint/training/train_magicpoint.py
@@ -215,7 +215,7 @@ def main(config_path: str):
                     #     epoch=state_ckpt_cb.epoch_start,
                     # )
                     TrainingOverAllLogger(
-                        tb_writer,
+                        create_tensorboard_writer(exp_dir, suffix="SEConvBlock_1"),
                         images=vis_batch["image"],
                         epoch=state_ckpt_cb.epoch_start
                     )
--- superpoint/utils/tensorboard.py
+++ superpoint/utils/tensorboard.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import tensorflow as tf
 
 
-def create_tensorboard_writer(log_dir: Path):
-    tb_dir = log_dir / "tensorboard"
+def create_tensorboard_writer(log_dir: Path, suffix=None):
+    tb_dir = (log_dir / "tensorboard") if not suffix else (log_dir / "tensorboard" / suffix)
     tb_dir.mkdir(parents=True, exist_ok=True)
     return tf.summary.create_file_writer(str(tb_dir))
