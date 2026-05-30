53d64a4628e412ca89c25c05d9611c03bb953b1d
sohaibwaheedgithub <sohaibwaheedgithub.mle@gmail.com>
2026-05-30 18:45:56 +0500

    added log for upwr

--- superpoint/training/train_magicpoint.py
+++ superpoint/training/train_magicpoint.py
@@ -178,14 +178,29 @@ def main(config_path: str):
     vis_batch = next(iter(valid_dataset.take(1)))
 
     if state_ckpt_cb.epoch_start == 1:
-        vis_batch_writer = create_tensorboard_writer(exp_dir, sub_dir="Input_Images")
+        vis_batch_writer = create_tensorboard_writer(exp_dir, sub_dir="Input")
         with vis_batch_writer.as_default():
             tf.summary.image(
-                "Visualization Batch",
+                "Images",
                 vis_batch["image"],
                 step=0,
                 max_outputs=4,
             )
+            
+            for i in range(vis_batch["image"].shape[0]):
+                tf.summary.histogram(
+                    f"Histogram Of Images",
+                    vis_batch["image"][i],
+                    step=i,
+                )
+
+                tf.summary.histogram(
+                    f"Histogram Of Standardized Images",
+                    (vis_batch["image"][i] - 0.5) / 0.5,
+                    step=i,
+                )
+
+
             vis_batch_writer.flush()
         del vis_batch_writer
 
